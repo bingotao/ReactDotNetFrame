729eb0de25d52fd7d4f11913b806da316ddf5968
韬 陈 <444034939@qq.com>
2016-11-01 12:59:40 +0800

    新增test项目 及less、jsx测试

--- JXGIS.ReactFrame.Web/App_Start/BundleConfig.cs
+++ JXGIS.ReactFrame.Web/App_Start/BundleConfig.cs
@@ -9,20 +9,25 @@ namespace JXGIS.ReactFrame.Web
 {
     public class BundleConfig
     {
-        private static string _extendPath = "~/Reference/";
+        private static string _refPath = "~/Reference/";
+
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //全局
-            bundles.Add(new Bundle("~/gStyle").Include(_extendPath + "antd-2.2.0/antd.min.css"));
+            //全局style
+            bundles.Add(new Bundle("~/gStyle").Include(
+                _refPath + "antd-2.2.0/antd.min.css"));
+            //全局script
             bundles.Add(new Bundle("~/gScripts").Include(
-                _extendPath + "react-15.3.2/react.min.js",
-                _extendPath + "react-15.3.2/react-dom.min.js",
-                _extendPath + "antd-2.2.0/antd.min.js"));
+                _refPath + "react-15.3.2/react.min.js",
+                _refPath + "react-15.3.2/react-dom.min.js",
+                _refPath + "antd-2.2.0/antd.min.js"));
 
 
 
-
+            bundles.Add(new LessBundle("~/Test/index/css").Include("~/Views/Test/css/index.less"));
+            bundles.Add(new BabelBundle("~/Test/index/js").Include("~/Views/Test/js/index.jsx"));
 
         }
+
     }
 }
